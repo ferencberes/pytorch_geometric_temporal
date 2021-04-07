260fe1eb24a4060c047f0ef708e2d2729dd10e19
Beres Ferenc <ferdzso05@gmail.com>
2021-04-07 20:31:24 +0000

    changing task from classification to regression

--- script/tennis_trial.py
+++ script/tennis_trial.py
@@ -1,16 +1,21 @@
-from torch_geometric_temporal.dataset import TwitterTennisDatasetLoader
+from torch_geometric_temporal.dataset import *
 from torch_geometric_temporal.signal import temporal_signal_split
+import numpy as np
 import sys
 
 event_id = sys.argv[1]
-if len(sys.argv) > 2:
-    N = int(sys.argv[2])
+N = int(sys.argv[2])
+offset = int(sys.argv[3])
+num_epochs = int(sys.argv[4])
+if len(sys.argv) > 5:
+    mode = sys.argv[5]
 else:
-    raise ValueError("Tennis data would be too large for the model. Use N=1000 or N=5000!")
-loader = TwitterTennisDatasetLoader(event_id, N)
+    mode = None
+
+loader = TwitterTennisDatasetLoader(event_id, N, mode, target_offset=offset)
 dataset = loader.get_dataset()
 
-train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.2)
+train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.5)
 
 import torch
 import torch.nn.functional as F
@@ -30,20 +35,39 @@ class RecurrentGCN(torch.nn.Module):
     
 from tqdm import tqdm
 
-model = RecurrentGCN(node_features = 2)
-
+if mode == "encoded":
+    model = RecurrentGCN(node_features = 16)
+elif mode == "diagonal":
+    model = RecurrentGCN(node_features = N)
+else:
+    model = RecurrentGCN(node_features = 2)
+    
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-
 model.train()
 
-for epoch in tqdm(range(100)):
+train_mse = []
+for epoch in tqdm(range(num_epochs)):
     cost = 0
+    epoch_ndcg = []
     for time, snapshot in enumerate(train_dataset):
         y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
         cost = cost + torch.mean((y_hat-snapshot.y)**2)
     cost = cost / (time+1)
+    train_mse.append(cost.detach().numpy())
     cost.backward()
     optimizer.step()
     optimizer.zero_grad()
+#print(train_mse)
+print("train mean mse:", np.mean(train_mse))
 
+test_mse = []
+cost = 0
+for time, snapshot in enumerate(test_dataset):
+    y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
+    new_cost = torch.mean((y_hat-snapshot.y)**2)
+    cost = cost + new_cost
+    test_mse.append(new_cost.detach().numpy())
+cost = cost / (time+1)
+print("test mse:", cost.detach().numpy())
+print(list(zip(range(60,120),test_mse)))
 print("done")
